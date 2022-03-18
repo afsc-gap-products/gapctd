3db5d6af7397467ac4f99db9cc61629e563c37ec
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-03-18 16:35:03 -0700

    Add loop edit and window filter modules to workflow to improve despiking

--- inst/extdata/SBE19plus/sbe19plus_getdata.bat
+++ inst/extdata/SBE19plus/sbe19plus_getdata.bat
@@ -4,9 +4,9 @@
 datcnv /i%1\data\*.hex /p%1\psa_xmlcon\DatCnv.psa /c%1\psa_xmlcon\%2 /o%1\cnv\ /a_raw
 @ Filter
 filter /i%1\cnv\*_raw.cnv /p%1\psa_xmlcon\Filter.psa /o%1\cnv\ /a_fil1 /c%1\psa_xmlcon\%2
-@ Align CTD casts - Does not work well -- Large spikes if used with celltm
-@ alignctd /i%1\cnv\*_fil1.cnv /p%1\psa_xmlcon\AlignCTD.psa /o%1\cnv\ /a_align /c%1\psa_xmlcon\%2
+@ Loop Edit
+loopedit  /i%1\cnv\*_fil1.cnv /p%1\psa_xmlcon\LoopEdit.psa /o%1\cnv\ /a_loopedit /c%1\psa_xmlcon\%2
 @ Cell Thermal Mass correction
-Celltm /i%1\cnv\*_fil1.cnv /p%1\psa_xmlcon\CellTM.psa /o%1\cnv\ /a_tmcorrect /c%1\psa_xmlcon\%2
-@ Cell Thermal Mass correction
-@ Celltm /i%1\cnv\*_align.cnv /p%1\psa_xmlcon\CellTM.psa /o%1\cnv\ /a_tmcorrect /c%1\psa_xmlcon\%2
+Celltm /i%1\cnv\*_loopedit.cnv /p%1\psa_xmlcon\CellTM.psa /o%1\cnv\ /a_tmcorrect /c%1\psa_xmlcon\%2
+@ Window filter
+Wfilter /i%1\cnv\*_tmcorrect.cnv /p%1\psa_xmlcon\W_Filter.psa /o%1\cnv\ /a_wfil /c%1\psa_xmlcon\%2
