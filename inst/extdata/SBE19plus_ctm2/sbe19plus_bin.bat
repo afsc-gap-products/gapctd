b33df548f8e9f538b62ce82bb8c0b3a89195b594
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-03-31 19:33:24 -0700

    Update batch and psa files for new workflows

---
+++ inst/extdata/SBE19plus_ctm2/sbe19plus_bin.bat
@@ -0,0 +1,5 @@
+@ SBE19plus V2 bin upcast and downcast data
+
+@ Bin cast data
+@ Binavg /i%1\cnv\*_downcast.cnv /p%1\psa_xmlcon\BinAvg.psa /o%1\cnv\ /a_binavg /c%1\psa_xmlcon\%2
+@Binavg /i%1\cnv\*_upcast.cnv /p%1\psa_xmlcon\BinAvg.psa /o%1\cnv\ /a_binavg /c%1\psa_xmlcon\%2
