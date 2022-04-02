86d2eb67e13cb3fdcd060af405f221fe8e3fb2bf
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-04-01 17:52:44 -0700

    Add density inversion test and file infrastructure

---
+++ R/correct_density_inversion.R
@@ -0,0 +1,113 @@
+#' Flag density inversion and correct
+#' 
+#' Screen profiles for density inversions and flag based on a threshold criteria based on density (kg m^-3) or Brunt-Vaisala buoyancy frequency (N^2), s^-2. Either  Optional: remove missing value and interpolate.
+#' 
+#' @param threshold Numerical. Threshold for flagging a density inversion. Must be negative. Default (-1e-5) is the buoyancy frequency threshold PMEL uses. GTSPP uses a density threshold of 0.05.
+#' @param threshold_method Character. "density" to flag inversions based on density anomalies, "bv" for Brunt-Vaisala buoyancy frequency
+#' @param correct_inversion Logical. Should density inversions be removed and interpolated?
+#' @return Writes flagged and corrected data to csvs in output/density_corrected.
+#' @export
+#' 
+
+correct_density_inversion <- function(threshold  = NULL, threshold_method = "bv", correct_inversion = TRUE) {
+  
+  threshold_method <- tolower(threshold_method)
+  
+  # Check that inputs are sensible
+  if(sign(threshold) == 1) {
+    stop("correct_density_inversion: Max inversion must be positive but is set to ", threshold, "!")
+  }
+  
+  if(threshold_method == "bv" & threshold < -1e-3) {
+    warning(paste0("correct_density_inversion: Brunt-Vaisala (N^2) threshold, (", threshold, ") is unusually low. Consider using a higher value (e.g. -1e-5)."))
+  }
+  
+  if(threshold_method == "density" & threshold > -0.01) {
+    warning(paste0("correct_density_inversion: Density threshold, (", threshold, ") is unusually high. Consider using a lower value (e.g., -0.03)."))
+  }
+  
+  # List final cnv_files
+  final_cnv_files <- list.files(here::here("final_cnv"), full.names = TRUE)
+  deploy <- list.files(here::here("final_cnv"))
+  
+  deploy_id <- sub("\\_raw.*", "", deploy)
+  deploy_id[grepl(pattern = "downcast", x = deploy)] <- paste0(deploy_id, "_downcast")[grepl(pattern = "downcast", x = deploy)] 
+  deploy_id[grepl(pattern = "upcast", x = deploy)] <- paste0(deploy_id, "_upcast")[grepl(pattern = "upcast", x = deploy)] 
+  
+  for(jj in 1:length(final_cnv_files)) {
+    
+    profile_oce <- try(oce::read.ctd(file = final_cnv_files[jj]), silent = TRUE)
+    
+    if(class(profile_oce) == "try-error") next
+    
+    # Calculate N2
+    profile_oce@data$N2 <- oce::swN2(profile_oce)
+    
+    
+    # Flag based on threshold method
+    flag_density <- switch(threshold_method,
+                           `bv` = {which(profile_oce@data$N2 < threshold)},
+                           `density` = {which(diff(profile_oce@data$gsw_densityA0[rank(profile_oce@data$pressure)]) < threshold)})
+    
+    # Counter to break the loop if automatic corrections get stuck
+    flag_loop <- 1
+    
+    while(length(flag_density) > 0 & flag_loop < 3) {
+      
+      if(threshold_method == "density") {
+        profile_oce@data$flag[rank(profile_oce@data$pressure)][flag_density+1] <- -1*(correct_inversion *-1)
+      } else if(threshold_method == "bv") {
+        # Flag but don't interpolate N2 anomalies at pressures < 20 db
+        profile_oce@data$flag[flag_density] <- 6*(correct_inversion)
+        profile_oce@data$flag[profile_oce@data$flag == 6*(correct_inversion) & profile_oce@data$pressure < 20] <- -6*(correct_inversion)
+      }
+      
+      flags <- which(profile_oce@data$flag == 6*(correct_inversion))
+      
+      
+      if(correct_inversion & length(flags) > 0) {
+        
+        profile_oce@data$temperature[flags] <- oce::oce.approx(x = profile_oce@data$depth[-flags], 
+                                                               y = profile_oce@data$temperature[-flags], 
+                                                               xout = profile_oce@data$depth[flags], 
+                                                               method = "unesco")
+        profile_oce@data$conductivity[flags] <- oce::oce.approx(x = profile_oce@data$depth[-flags], 
+                                                                y = profile_oce@data$conductivity[-flags], 
+                                                                xout = profile_oce@data$depth[flags], 
+                                                                method = "unesco")
+        profile_oce@data$salinity[flags] <- oce::oce.approx(x = profile_oce@data$depth[-flags], 
+                                                            y = profile_oce@data$salinity[-flags], 
+                                                            xout = profile_oce@data$depth[flags], 
+                                                            method = "unesco")
+        profile_oce@data$soundSpeed[flags] <- oce::oce.approx(x = profile_oce@data$depth[-flags], 
+                                                              y = profile_oce@data$soundSpeed[-flags], 
+                                                              xout = profile_oce@data$depth[flags], 
+                                                              method = "unesco")
+        profile_oce@data$gsw_saA0[flags] <- oce::oce.approx(x = profile_oce@data$depth[-flags], 
+                                                            y = profile_oce@data$gsw_saA0[-flags], 
+                                                            xout = profile_oce@data$depth[flags], 
+                                                            method = "unesco")
+        profile_oce@data$gsw_densityA0[flags] <- oce::oce.approx(x = profile_oce@data$depth[-flags], 
+                                                                 y = profile_oce@data$gsw_densityA0[-flags], 
+                                                                 xout = profile_oce@data$depth[flags], 
+                                                                 method = "unesco")
+        
+        # Re-calculate N2
+        profile_oce@data$N2 <- oce::swN2(profile_oce)
+        
+        # Re-flag
+        flag_density <- switch(threshold_method,
+                               `bv` = {which(profile_oce@data$N2 < threshold & profile_oce@data$pressure > 20)},
+                               `density` = {which(diff(profile_oce@data$gsw_densityA0[rank(profile_oce@data$pressure)]) < threshold)})
+      } else {
+        flag_density <- 0
+      }
+      
+      flag_loop <- flag_loop + 1
+    }
+    
+    # Write corrected profile to output/density_corrected
+    oce::write.ctd(object = profile_oce, file = here::here("output", "density_corrected", paste0(deploy_id[jj], ".csv")), metadata = FALSE, format = "csv")
+    
+  }
+}
--- R/setup_ctd_processing_dir.R
+++ R/setup_ctd_processing_dir.R
@@ -46,21 +46,17 @@ setup_ctd_processing_dir <- function(ctd_dir,
   if(!dir.exists("output")) {
     dir.create("output")
   }
-  
+
+  if(!dir.exists(here::here("output", "density_corrected"))) {
+    dir.create(here::here("output",  "density_corrected"))
+  }
+    
   if(!dir.exists(here::here("output", "manual_flag"))) {
     dir.create(here::here("output", "manual_flag"))
   }
   
   if(!dir.exists(here::here("output", "accepted_profiles"))) {
     dir.create(here::here("output", "accepted_profiles"))
-  }
-  
-  if(!dir.exists(here::here("output", "binavg"))) {
-    dir.create(here::here("output", "binavg"))
-  }
-  
-  if(!dir.exists(here::here("output", "raw"))) {
-    dir.create(here::here("output", "raw"))
   }
   
   if(!dir.exists("plots")) {
