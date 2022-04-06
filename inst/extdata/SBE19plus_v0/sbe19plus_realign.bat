8f53495c7896631c3e3b0a32e87a6942f34685e6
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-04-06 12:05:15 -0700

    Update batch files for estimation of celltm parameters

--- inst/extdata/SBE19plus_v0/sbe19plus_realign.bat
+++ inst/extdata/SBE19plus_v0/sbe19plus_realign.bat
@@ -1,5 +1,2 @@
 @ Align
 alignctd /i%3 /p%1\psa_xmlcon\AlignCTD.psa /o%1\cnv\ /a%4 /c%1\psa_xmlcon\%2
-Celltm /i%5 /p%1\psa_xmlcon\CellTM.psa /o%1\cnv\ /a_tmcorrect /c%1\psa_xmlcon\%2
-@ Loop Edit
-loopedit /i%6 /p%1\psa_xmlcon\LoopEdit.psa /o%1\cnv\ /a_loopedit /c%1\psa_xmlcon\%2
