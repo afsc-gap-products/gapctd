34eff686146a7979e7db36de1cf14a588f39f373
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-09-10 12:58:41 -0700

    Change module order

--- inst/extdata/SBE19plus_v0/sbe19plus_getdata.bat
+++ inst/extdata/SBE19plus_v0/sbe19plus_getdata.bat
@@ -8,5 +8,3 @@ Wfilter /i%1\cnv\*_raw.cnv /p%1\psa_xmlcon\W_Filter.psa /o%1\cnv\ /a_wfil /c%1\p
 filter /i%1\cnv\*_wfil.cnv /p%1\psa_xmlcon\Filter.psa /o%1\cnv\ /a_fil1 /c%1\psa_xmlcon\%2
 @ Loop Edit
 loopedit  /i%1\cnv\*_fil1.cnv /p%1\psa_xmlcon\LoopEdit.psa /o%1\cnv\ /a_loopedit /c%1\psa_xmlcon\%2
-@ Cell Thermal Mass correction
-@ Celltm /i%1\cnv\*_loopedit.cnv /p%1\psa_xmlcon\CellTM.psa /o%1\cnv\ /a_tmcorrect /c%1\psa_xmlcon\%2
--- inst/extdata/SBE19plus_v2/sbe19plus_derive.bat
+++ inst/extdata/SBE19plus_v2/sbe19plus_derive.bat
@@ -2,7 +2,7 @@
 @ Derive EOS80 and TEOS10
 
 @ Derive EOS-80
-Derive /i%1\cnv\*_loopedit.cnv /p%1\psa_xmlcon\Derive.psa /o%1\cnv\ /a_EOS80 /c%1\psa_xmlcon\%2
+Derive /i%1\cnv\%3 /p%1\psa_xmlcon\Derive.psa /o%1\cnv\ /a_EOS80 /c%1\psa_xmlcon\%2
 @ Thermodynamic Equation of Seawater
 DeriveTEOS10 /i%1\cnv\*_EOS80.cnv /p%1\psa_xmlcon\DeriveTEOS10.psa /o%1\cnv\ /a_TEOS10 /c%1\psa_xmlcon\%2
 @ Split upcast and downcast
