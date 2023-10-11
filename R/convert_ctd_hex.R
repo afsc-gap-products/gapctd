0e94824caaec7f0c63c569599ccb319fbff4ed61
sean-rohan-NOAA <sean.rohan@noaa.gov>
2023-10-11 13:48:38 -0700

    Remove duplicate convert_ctd_hex

--- R/convert_ctd_hex.R
+++ R/convert_ctd_hex.R
@@ -2,47 +2,78 @@
 #' 
 #'  Converts .hex files to .cnv files using SBE Data Processing. Must have SBE Data Processing software installed for this function to work.
 #'  
-#'  @param ctd_directory_path Path to CTD directory
-#'  @param xmlcon_path Path to xmlcon file
-#'  @export
+#' @param ctd_directory_path Path to CTD directory
+#' @param xmlcon_path Path to xmlcon file
+#' @export
+#' @examples
+#' # Copy system files to working directory for example
+#' file.copy(from = system.file("exdata/CTDtoBTD/2021_06_13_0003.hex", package = "GAPsurvey"),
+#' to = gsub(pattern = system.file("exdata/CTDtoBTD/", package = "GAPsurvey"),
+#'           replacement = getwd(),
+#'           x = system.file("exdata/CTDtoBTD/2021_06_13_0003.hex", package = "GAPsurvey")))
+#'
+#' file.copy(from = system.file("exdata/CTDtoBTD/19-8102_Deploy2021.xmlcon", package = "GAPsurvey"),
+#'           to = gsub(pattern = system.file("exdata/CTDtoBTD/", package = "GAPsurvey"),
+#'                     replacement = getwd(),
+#'                     x = system.file("exdata/CTDtoBTD/19-8102_Deploy2021.xmlcon", package = "GAPsurvey")))
+#'
+#' # Run convert_ctd_hex()
+#' GAPsurvey:::convert_ctd_hex(hex_file_path = "2021_06_13_0003.hex",
+#'                             xmlcon_path = "19-8102_Deploy2021.xmlcon",
+#'                             bat_file = NULL,
+#'                             datcnv_file = NULL)
 
-convert_ctd_hex <- function(ctd_directory_path, 
-                            xmlcon_path, 
-                            bat_file = NULL, 
+convert_ctd_hex <- function(hex_file_path,
+                            xmlcon_path,
+                            bat_file = NULL,
                             datcnv_file = NULL) {
   
+  # Replace double backslashes in filepaths with forward slashes
+  hex_file_path <- gsub(pattern = "\\\\", replacement = "/", x = hex_file_path)
+  xmlcon_path<- gsub(pattern = "\\\\", replacement = "/", x = xmlcon_path)
+  
+  # Handle case where file is in the root directory
+  if(dirname(hex_file_path) == ".") {
+    hex_file_path <- paste0(getwd(), "/", hex_file_path)
+  }
+  
+  if(dirname(xmlcon_path) == ".") {
+    xmlcon_path <- paste0(getwd(), "/", xmlcon_path)
+  }
+  
+  if(!file.exists(hex_file_path)) {
+    stop("convert_ctd_hex: Specified hex_file_path (", hex_file_path,  ") does not exist. Please replace with a valid path to a .hex file.")
+  }
+  
+  message("convert_ctd_hex: Attempting to convert ", hex_file_path, " to .cnv")
+  
+  out_path <- gsub(pattern = ".hex", replacement = "_raw.cnv", x = hex_file_path)
+  
+  # Specify paths to .bat and .psa files for SBE data processing
   if(is.null(bat_file)) {
-    bat_file <- system.file("extdata/atsea/atsea_getdata.bat", package = "gapctd")    
+    bat_file <- system.file("exdata/CTDtoBTD/atsea_getdata.bat", package = "GAPsurvey")
   }
   
   if(is.null(datcnv_file)) {
-    datcnv_file <- system.file("extdata/atsea/DatCnv.psa", package = "gapctd")
+    datcnv_file <- system.file("exdata/CTDtoBTD/DatCnv.psa", package = "GAPsurvey")
   }
   
-  if(!dir.exists(paste0(getwd(), "/output"))) {
-    dir.create(paste0(getwd(), "/output"))
-  }
-  
-  if(!dir.exists(paste0(getwd(), "/data"))) {
-    dir.create(paste0(getwd(), "/data"))
-  }
-  
-  # Generate list of files
-  hex_file_paths <- list.files(path = ctd_directory_path, 
-                               pattern = ".hex",
-                               full.names = TRUE)
-  
-  file.copy(from = hex_file_paths,
-            to = gsub(pattern = ctd_directory_path, 
-                      replacement = paste0(getwd(), "/data"),
-                      x = hex_file_paths))
-  
-  # Run batch processing to generate raw .cnv files
-  system(command = paste0("sbebatch \"", 
-                          bat_file, "\" \"", 
-                          getwd(), "\" \"", 
+  # Run SBE data processing
+  system(command = paste0("sbebatch \"",
+                          bat_file, "\" \"",
+                          hex_file_path, "\" \"",
                           datcnv_file, "\" \"",
-                          xmlcon_path, "\""))
+                          xmlcon_path, "\" \"",
+                          sub("/[^/]+$", "", hex_file_path), "\""
+  ))
+  
+  if(file.exists(out_path)) {
+    message("convert_ctd_hex: .cnv file created at ", out_path, ".")
+  } else {
+    stop("convert_ctd_hex: Failed to convert .hex to .cnv.")
+  }
+  
+  return(out_path)
 }
 
 
