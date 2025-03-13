1c2ad7e9f2281cbd6dea4580035781ad4535edac
NicoleCharriere-NOAA <nicole.charriere@noaa.gov>
2025-03-13 16:47:43 -0700

    Deleted extra parentheses and added row.names check.

--- R/hex_xmlcon_filter.R
+++ R/hex_xmlcon_filter.R
@@ -14,13 +14,14 @@ Filters_hex <- rbind(Filters_baseR, add_hex_filter)
 
 ## Add .xmlcon file to newly created Filter_hex matrix
 
-add_xmlcon_filter <- c(("Sea-Bird Instrument Configuration (.xmlcon)", "*.xmlcon")
+add_xmlcon_filter <- c("Sea-Bird Instrument Configuration (.xmlcon)", "*.xmlcon")
 Filters_hex_xmlcon <- rbind(Filters_hex, add_xmlcon_filter)
 
 ## Cosmetic: Reorder row names in Filters_hex_xmlcon so "All Files" appears at bottom of final Filter matrix for traditional Windows file appearance
 
 row.names(Filters_hex_xmlcon)
 Filters_hex_xmlcon_reorder <- Filters_hex_xmlcon[c(1,2,3,4,5,6,7,8,9,10,11,13,14,12),]
+row.names(Filters_hex_xmlcon_reorder)
 
 ##Test file selection capability (choose.files) with Filters_hex_xmlcon_reorder
 
